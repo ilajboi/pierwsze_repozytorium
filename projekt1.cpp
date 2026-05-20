354bc342eaf2ce62f1dd21f580532b8e0f769183
ilajboi <konrad.michalczyk@yahoo.com>
2026-05-20 16:14:48 +0000

    Dodanie funkcji wyznaczajacej silnie

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+// Polecenie 3
+long long obliczSilnie(int n) {
+    if (n < 0) return 0; 
+    long long wynik = 1;
+    for (int i = 1; i <= n; i++) {
+        wynik *= i;
+    }
+    return wynik;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
