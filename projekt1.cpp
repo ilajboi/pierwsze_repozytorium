fded9c24e90798277862c6632fcdb345a6d2812e
ilajboi <konrad.michalczyk@yahoo.com>
2026-05-20 16:22:12 +0000

    Rozbudowa menu o opcje obliczania silni

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// Polecenie 3
+// Funkcja wyznaczająca silnię liczby (dodana w Poleceniu 3)
 long long obliczSilnie(int n) {
     if (n < 0) return 0; 
     long long wynik = 1;
@@ -22,8 +22,17 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "1. Oblicz silnie dla liczby a" << endl; // Nowa opcja w menu
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+
+        // Obsługa wyboru użytkownika
+        if (wyjscie == 1) {
+            cout << "Silnia z liczby " << a << " wynosi: " << obliczSilnie(a) << endl;
+        } else if (wyjscie != 0) {
+            cout << "Niepoprawny wybor!" << endl;
+        }
+
     } while(wyjscie != 0);
     return 0;
 }
